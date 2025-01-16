f4cce53661891916b803b6f33593acaec2cb9017
Vivek <96946822+Vivek13130@users.noreply.github.com>
2025-01-16 21:31:31 +0530

    commit 2
    player set up done , now coding the main part

---
+++ scenes/character_panel.gd
@@ -0,0 +1,10 @@
+extends Control
+@onready var character: Sprite2D = $Character
+
+var viewport_width
+
+func _ready() -> void:
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
